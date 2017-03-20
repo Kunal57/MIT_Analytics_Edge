1368dd270c868639287d10dcfe8061f2fcf3c1b8
Kunal Patel <kunalpatelx92@gmail.com>
2017-03-20 17:24:55 -0500

    Create dataframe for the dataset and call the str and summary functions

---
+++ unit_1/assignment_1/Assignment_1.R
@@ -0,0 +1,14 @@
+# Assignment 1 - An Analytical Detective
+
+# Get working directory
+  getwd()
+# Set working directory
+  setwd('/Users/Kunal/Development/MIT_Analytics_Edge/unit_1/assignment_1')
+# Set dataset to a dataframe (mvt)
+  mvt = read.csv('mvtWeek1.csv')
+# Call str function on the dataframe
+  str(mvt)
+# Call summary function on the dataframe
+  summary(mvt)
+  
+  
